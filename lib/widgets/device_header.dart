db265eaf51731a409702376c96170a163a708bd3
Anthony Doud <anthonydoud97@gmail.com>
2024-03-01 10:56:10 -0600

    added functions back into device_header

--- lib/widgets/device_header.dart
+++ lib/widgets/device_header.dart
@@ -35,7 +35,6 @@ class DeviceHeader extends StatefulWidget {
   final BluetoothDevice device;
   final BLEData bleData;
   final bool connectOnly;
-
   const DeviceHeader({Key? key, required this.device, required this.bleData, this.connectOnly = false})
       : super(key: key);
 
@@ -44,6 +43,185 @@ class DeviceHeader extends StatefulWidget {
 }
 
 class _DeviceHeaderState extends State<DeviceHeader> {
+  StreamSubscription<BluetoothConnectionState>? _connectionStateSubscription;
+  Timer rssiTimer = Timer.periodic(Duration(seconds: 30), (rssiTimer) {});
+
+  @override
+  void initState() {
+    super.initState();
+    _connectionStateSubscription = widget.device.connectionState.listen((state) async {
+      if (widget.device.isConnected) {
+        widget.bleData.rssi.value = await widget.device.readRssi();
+      } else {
+        widget.bleData.rssi.value = 0;
+      }
+      if (mounted) {
+        setState(() {});
+      }
+      rssiTimer = Timer.periodic(Duration(seconds: 20), (rssiTimer) async {
+        if (widget.device.isConnected) {
+          try {
+            widget.bleData.rssi.value = await widget.device.readRssi();
+            if (widget.bleData.firmwareVersion == "") {
+              widget.bleData.customCharacteristic
+                  .forEach((d) => (d["vName"] == fwVname) ? widget.bleData.firmwareVersion = d["value"] ?? "" : null);
+            }
+          } catch (e) {
+            widget.bleData.rssi.value = 0;
+          }
+          if (mounted) {
+            setState(() {});
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _connectionStateSubscription!.cancel();
+    rssiTimer.cancel();
+
+    super.dispose();
+  }
+
+  bool get isConnected {
+    return widget.bleData.connectionState == BluetoothConnectionState.connected;
+  }
+
+  Future onConnectPressed() async {
+    try {
+      await widget.device.connectAndUpdateStream();
+      Snackbar.show(ABC.c, "Connect: Success", success: true);
+      await onDiscoverServicesPressed();
+    } catch (e) {
+      if (e is FlutterBluePlusException && e.code == FbpErrorCode.connectionCanceled.index) {
+        // ignore connections canceled by the user
+      } else {
+        Snackbar.show(ABC.c, prettyException("Connect Error:", e), success: false);
+      }
+    }
+  }
+
+  Future onDisconnectPressed() async {
+    try {
+      await widget.device.disconnectAndUpdateStream();
+      Snackbar.show(ABC.c, "Disconnect: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Disconnect Error:", e), success: false);
+    }
+  }
+
+  Future onDiscoverServicesPressed() async {
+    if (mounted) {
+      setState(() {
+        widget.bleData.isReadingOrWriting.value = true;
+      });
+    }
+    try {
+      widget.bleData.services = await widget.device.discoverServices();
+      //await _findChar();
+      await updateCustomCharacter(widget.bleData, widget.device);
+      Snackbar.show(ABC.c, "Discover Services: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Discover Services Error:", e), success: false);
+    }
+    if (mounted) {
+      setState(() {
+        widget.bleData.isReadingOrWriting.value = false;
+      });
+    }
+  }
+
+  Future onSaveSettingsPressed() async {
+    try {
+      await saveAllSettings(widget.bleData, widget.device);
+      Snackbar.show(ABC.c, "Settings Saved", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Save Settings Failed ", e), success: false);
+    }
+  }
+
+  Future onSaveLocalPressed() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('user', jsonEncode(widget.bleData.customCharacteristic));
+      Snackbar.show(ABC.c, "Settings Saved", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Save Local Failed ", e), success: false);
+    }
+  }
+
+  Future onLoadLocalPressed() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      widget.bleData.customCharacteristic = jsonDecode(prefs.getString('user')!);
+      Snackbar.show(ABC.c, "Settings Loaded", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Load local failed. Do you have a backup?", e), success: false);
+    }
+  }
+
+  Future onRebootPressed() async {
+    try {
+      await reboot(widget.bleData, widget.device);
+      Snackbar.show(ABC.a, "SmartSpin2k is rebooting", success: true);
+      await onDisconnectPressed();
+      await onConnectPressed();
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Reboot Failed ", e), success: false);
+    }
+  }
+
+  Future onResetPressed() async {
+    try {
+      await resetToDefaults(widget.bleData, widget.device);
+      await discoverServices();
+      Snackbar.show(ABC.c, "SmartSpin2k has been reset to defaults", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Reset Failed ", e), success: false);
+    }
+  }
+
+  Future discoverServices() async {
+    if (mounted) {
+      setState(() {
+        widget.bleData.isReadingOrWriting.value = true;
+      });
+    }
+    if (widget.device.isConnected) {
+      try {
+        widget.bleData.services = await widget.device.discoverServices();
+        //_findChar();
+        await updateCustomCharacter(widget.bleData, widget.device);
+        Snackbar.show(ABC.c, "Discover Services: Success", success: true);
+      } catch (e) {
+        Snackbar.show(ABC.c, prettyException("Discover Services Error:", e), success: false);
+      }
+    }
+    if (mounted) {
+      setState(() {
+        widget.bleData.isReadingOrWriting.value = false;
+      });
+    }
+  }
+
+  Widget buildSpinner(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: CircularProgressIndicator(
+          //backgroundColor: Colors.black12,
+          //color: Colors.black26,
+          ),
+    );
+  }
+
+  Widget buildRemoteId(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('${widget.device.remoteId}'),
+    );
+  }
   bool _isExpanded = false;
 
   Widget _buildSignalStrengthIcon(int rssi) {
@@ -76,13 +254,13 @@ class _DeviceHeaderState extends State<DeviceHeader> {
       AnimatedCrossFade(
         firstChild: Container(height: 0),
         secondChild: Column(children: <Widget>[
-          _buildActionButton('Connect', FontAwesomeIcons.plug, _onConnectPressed),
-          _buildActionButton('Refresh', FontAwesomeIcons.syncAlt, _onDiscoverServicesPressed),
-          _buildActionButton('Reboot SS2K', FontAwesomeIcons.redo, _onRebootPressed),
-          _buildActionButton('Set Defaults', FontAwesomeIcons.undo, _onResetPressed),
-          _buildActionButton('Save To SS2k', FontAwesomeIcons.save, _onSaveSettingsPressed),
-          _buildActionButton('Backup Settings', FontAwesomeIcons.solidSave, _onSaveLocalPressed),
-          _buildActionButton('Load Backup', FontAwesomeIcons.upload, _onLoadLocalPressed),
+          _buildActionButton('Connect', FontAwesomeIcons.plug, onConnectPressed),
+          _buildActionButton('Refresh', FontAwesomeIcons.rotate, onDiscoverServicesPressed),
+          _buildActionButton('Reboot SS2K', FontAwesomeIcons.arrowRotateRight, onRebootPressed),
+          _buildActionButton('Set Defaults', FontAwesomeIcons.arrowRotateLeft, onResetPressed),
+          _buildActionButton('Save To SS2k', FontAwesomeIcons.floppyDisk, onSaveSettingsPressed),
+          _buildActionButton('Backup Settings', FontAwesomeIcons.solidFloppyDisk, onSaveLocalPressed),
+          _buildActionButton('Load Backup', FontAwesomeIcons.upload, onLoadLocalPressed),
         ]),
         crossFadeState: _isExpanded ? CrossFadeState.showSecond : CrossFadeState.showFirst,
         duration: Duration(milliseconds: 500),
