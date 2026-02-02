30621cee2c47096a593b1eb056e8b0ff831540ae
Anthony Doud <anthonydoud97@gmail.com>
2026-02-01 21:24:14 -0600

    automatic build work.

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,9 +9,28 @@ cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cl
 # Export environment variables
 export STRAVA_CLIENT_ID=$STRAVA_CLIENT_ID
 export STRAVA_CLIENT_SECRET=$STRAVA_CLIENT_SECRET
+export INTERVALS_CLIENT_ID=$INTERVALS_CLIENT_ID
+export INTERVALS_CLIENT_SECRET=$INTERVALS_CLIENT_SECRET
+
+# Create env.local.dart
+mkdir -p lib/config
+cat > lib/config/env.local.dart <<EOF
+class Environment {
+  static const String stravaClientId = '$STRAVA_CLIENT_ID';
+  static const String stravaClientSecret = '$STRAVA_CLIENT_SECRET';
+  static const String intervalsClientId = '$INTERVALS_CLIENT_ID';
+  static const String intervalsClientSecret = '$INTERVALS_CLIENT_SECRET';
+
+  static bool get hasStravaConfig =>
+    stravaClientId.isNotEmpty && stravaClientSecret.isNotEmpty;
+
+  static bool get hasIntervalsConfig =>
+    intervalsClientId.isNotEmpty && intervalsClientSecret.isNotEmpty;
+}
+EOF
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b 3.22.3 $HOME/flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
